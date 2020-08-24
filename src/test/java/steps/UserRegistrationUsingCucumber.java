d52d0a2266237d68ab49fcd91b28a4bbb9b73a97
hala-samir <h.samir2011@gmail.com>
2020-08-24 21:56:46 +0200

    registration using cucumber

--- src/test/java/steps/UserRegistrationUsingCucumber.java
+++ src/test/java/steps/UserRegistrationUsingCucumber.java
@@ -1,4 +1,4 @@
-package stepDefinitions;
+package steps;
 
 import org.testng.Assert;
 
@@ -13,26 +13,32 @@ public class UserRegistrationUsingCucumber extends TestBase {
 	HomePage homePageObj;
 	UserRegistration registPageObj;	
 	
-	@Given("The user is in homepage")
+	@Given("^The user is in homepage$")
 	public void the_user_is_in_homepage() 
 	{
 		homePageObj= new HomePage(driver);
 		homePageObj.openRegistrationPage();
 	}
-	@When("User clicks registartion link")
+	@When("^User clicks registartion link$")
 	public void user_clicks_registartion_link() 
 	{  	
 		Assert.assertTrue(driver.getCurrentUrl().contains("register"));   
 	}
 	
-	@When("User enters correct data")
+	/*@When("User enters correct data")
 	public void he_enters_correct_data() 
 	{
 		registPageObj=new UserRegistration(driver);
 		registPageObj.userRegisteration("ccc", "ddd", "teetts3@sss.cc", "123456789");	
+	}*/
+	@When("^User enters correct \"([^\"]*)\", \"([^\"]*)\", \"([^\"]*)\", \"([^\"]*)\"$")
+	public void user_enters_correct(String firstName, String lastName, String email, String password) 
+	{
+		registPageObj=new UserRegistration(driver);
+		registPageObj.userRegisteration(firstName, lastName, email, password);   
 	}
 	
-	@Then("User can registered successfully")
+	@Then("^User can registered successfully$")
 	public void user_can_registared_successfully() 
 	{
 		registPageObj=new UserRegistration(driver);
--- target/test-classes/features/userRegistration.feature
+++ target/test-classes/features/userRegistration.feature
@@ -1,8 +1,12 @@
 Feature: User Registartion
 I want to check that the user can registeration 
 
-Scenario:  user can register successfully
+Scenario Outline:  user can register successfully
 Given The user is in homepage
 When User clicks registartion link
-And User enters correct data
+And User enters correct "<firstName>" , "<lastName>" , "<email>" , "<password>"
 Then User can registered successfully
+
+Examples: 
+ | firstName | lastName | email | password |
+ | Hala | Samir | tttest@test1.ccm | 123456789 |
