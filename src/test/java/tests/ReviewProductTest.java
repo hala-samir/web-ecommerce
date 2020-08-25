fb3d9fe2567ec4db02da8492e6c371df5fcb78f7
hala-samir <h.samir2011@gmail.com>
2020-08-25 19:24:33 +0200

    Addin Allure report

--- src/test/java/tests/ReviewProductTest.java
+++ src/test/java/tests/ReviewProductTest.java
@@ -3,6 +3,9 @@ package tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
 import pages.EmailFriendPage;
 import pages.HomePage;
 import pages.LoginPage;
@@ -19,27 +22,27 @@ public class ReviewProductTest extends TestBase {
 	UserRegistration registPageObj;
 	LoginPage loginObj;
 	ProductReviewPage productReviewPageObj;
-	
+
 	String productName = "Apple MacBook Pro 13-inch";
 	String productPartialName = "Apple Mac";
 	String fname="Tester";
 	String lname="Test";
-	String email="tester@ffeg6.co0m";
+	String email="tester@ffeg9.co0m";
 	String friend_mail="friend1@gmail.com";
 	String oldPass="123456789";
 	String msg= "check this please!!";
 	String review_title= "Good product!";
 	String review_txt= "It can be better";
-	
-	
+
+
 	@Test(priority=1, alwaysRun = true)
 	public void userCanRegisterSuccessfully() 
 	{	homeObj=new HomePage(driver);
-		registPageObj=new UserRegistration(driver);
-		
-		homeObj.openRegistrationPage();
-		registPageObj.userRegisteration(fname, lname, email, oldPass);
-		Assert.assertTrue(registPageObj.successMsg.getText().contains("Your registration completed"));
+	registPageObj=new UserRegistration(driver);
+
+	homeObj.openRegistrationPage();
+	registPageObj.userRegisteration(fname, lname, email, oldPass);
+	Assert.assertTrue(registPageObj.successMsg.getText().contains("Your registration completed"));
 	}
 
 	//@Test(dependsOnMethods="userCanRegisterSuccessfully")
@@ -47,13 +50,15 @@ public class ReviewProductTest extends TestBase {
 	public void userSearchForProductsUsingAutoSuggestion() 
 	{
 		productPageObj = new ProductPage(driver);
-		
+
 		homeObj.searchProductUsingAutoSuggestion(productPartialName);
 		Assert.assertEquals(productPageObj.productName.getText(), productName);
 	}
-	
+
 	//@Test(dependsOnMethods="userSearchForProductsUsingAutoSuggestion")
 	@Test(priority=3, alwaysRun = true) 
+	@Severity(SeverityLevel.CRITICAL)
+	@Description("Product Review")
 	public void userCanSubmitProductReview() 
 	{
 		productReviewPageObj= new ProductReviewPage(driver);
