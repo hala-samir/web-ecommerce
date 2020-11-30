6ef2e8a1af20bea3be87ae00a72be9693cf432d0
hala-samir <h.samir2011@gmail.com>
2020-11-30 11:30:38 +0200

    run tests on chrome headless and update chrome driver

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,17 +1,18 @@
 package tests;
 
+
 import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
-
-
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriverService;
 import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
@@ -27,8 +28,8 @@ public class TestBase extends AbstractTestNGCucumberTests
 {
 	public static WebDriver driver;
 	public static String downloadPath=System.getProperty("user.dir")+"/downloads";
-	
-	
+
+
 	public static FirefoxOptions firefoxOption() 
 	{
 		FirefoxOptions option= new FirefoxOptions();
@@ -57,15 +58,40 @@ public class TestBase extends AbstractTestNGCucumberTests
 		if (browserName.equalsIgnoreCase("Firefox")) 
 		{
 			System.setProperty("webdriver.gecko.driver",System.getProperty("user.dir")+"/drivers/geckodriver.exe");
-			
+
 			driver=new FirefoxDriver(firefoxOption());	
 			//driver= new FirefoxDriver();
+			System.out.println("yalahwiiiiiiii2");
+
+		}
+		else if(browserName.equalsIgnoreCase("headless")) 
+		{
+			DesiredCapabilities caps = new DesiredCapabilities();
+			caps.setJavascriptEnabled(true);
+			caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, System.getProperty("user.dir")+"/drivers/phantomjs.exe");
+
+			driver = new PhantomJSDriver(caps);
+			String [] phantomJsArgs = {"--web-security=no", "--ignore-ssl-errors=yes"};
+			caps.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, phantomJsArgs);
+			driver=new PhantomJSDriver(caps);
+			System.out.println("yalahwiiiiiiii");
+
 		}
 		else if (browserName.equalsIgnoreCase("chrome"))
 		{
-			System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"/drivers/chromedriver.exe");
+			System.setProperty("webdriver.chrome.driver",
+					System.getProperty("user.dir")+"/drivers2/chromedriver.exe");
 			driver=new ChromeDriver(chromeOption());
-		//	driver=new ChromeDriver();
+			//	driver=new ChromeDriver();
+		}
+		else if (browserName.equalsIgnoreCase("chromeHeadless"))
+		{
+			System.setProperty("webdriver.chrome.driver",
+					System.getProperty("user.dir")+"/drivers2/chromedriver.exe");
+
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("--headless");
+			driver= new ChromeDriver(options);			
 		}
 		else if (browserName.equalsIgnoreCase("ie"))
 		{
@@ -73,10 +99,11 @@ public class TestBase extends AbstractTestNGCucumberTests
 			driver=new InternetExplorerDriver();
 		}
 
+
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
 		driver.navigate().to("https://demo.nopcommerce.com/");
-	
+
 	}
 
 	@AfterSuite
